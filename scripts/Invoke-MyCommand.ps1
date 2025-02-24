1a6eda8d87252ce9307fa0070fc3dcc2c93a3803
peeyushtrivedi0706 <33380085+peeyushtrivedi0706@users.noreply.github.com>
2025-02-24 15:59:43 +0530

    Create Invoke-MyCommand.ps1

---
+++ scripts/Invoke-MyCommand.ps1
@@ -0,0 +1,38 @@
+param(
+    [string]$ComputerName,
+    [Parameter]
+    [scriptblock]$ScriptBlock,
+    [string] $FilePath,
+    $ArgumentList
+    )   
+
+# Execute the command locally or remotely
+if ($ComputerName -eq "localhost") {  
+    if($FilePath -ne $null) {
+        if($ArgumentList -ne $null) {
+            . $FilePath @ArgumentList
+        } else {
+            . $FilePath
+        }
+    } else {
+      if($ArgumentList -ne $null) {
+            & $ScriptBlock @ArgumentList
+        } else {
+            & $ScriptBlock
+        }
+    }     
+} else {
+    if($FilePath -ne $null) {
+        if($ArgumentList -ne $null) {
+            Invoke-Command -ComputerName $ComputerName -FilePath $FilePath -ArgumentList $ArgumentList
+        } else {
+            Invoke-Command -ComputerName $ComputerName -FilePath $FilePath
+        }
+    } else {
+        if($ArgumentList -ne $null) {
+            Invoke-Command -ComputerName $ComputerName -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList
+        } else {
+            Invoke-Command -ComputerName $ComputerName -ScriptBlock $ScriptBlock
+        }
+    }
+}
